3d84ccb596e28c01cea2e40f43e46662cc097948
Anthony Joel <anto777molina777@gmail.com>
2022-09-20 12:28:16 -0500

    ddbdd

--- database/db.sql
+++ database/db.sql
@@ -3,8 +3,8 @@ CREATE DATABASE rol_jugador;
 USE rol_jugador;
 
 CREATE TABLE users(
-    id INT(11) NOT NULL
-    username VARCHAR(16) NOT NULL
+    id INT(11) NOT NULL,
+    username VARCHAR(16) NOT NULL,
     password VARCHAR(60) NOT NULL, 
     fullname VARCHAR(100) NOT NULL
 );
@@ -12,27 +12,13 @@ CREATE TABLE users(
 ALTER TABLE users
     ADD PRIMARY KEY (id);
 
-ALTER TABLE users 
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+ALTER TABLE users
+MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
-
--- LINKS TABLES
-CREATE TABLE links (
-    id INT(11) NOT NULL,
-    title VARCHAR(150) NOT NULL,
-    url VARCHAR(255) NOT NULL, 
-    description TEXT,
-    user_id INT(11),
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
-    );
-
-    ALTER TABLE links
-        ADD PRIMARY KEY (id);
-
-    ALTER TABLE links
-        MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+DESCRIBE users;
     
-    DESCRIBE links;
+
+
+
 
 
