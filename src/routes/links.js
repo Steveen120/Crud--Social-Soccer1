44b373ccca43a7f41eb9a848dc646dc7de9693ba
Anthony Joel <anto777molina777@gmail.com>
2022-09-21 10:29:20 -0500

    tablas creadas

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,12 +3,38 @@ const router  = express.Router();
 
 const pool = require('../database');
 
-router.get('/add', (req, res)  => {
-    res.render('links/add');
+//jugador//
+router.get('/jugador', (req, res)  => {
+    res.render('links/jugador');
 });
 
-router.post('/add', (req, res ) => {
+router.post('/jugador', (req, res ) => {
+    res.send('received')
+});
+//equipo//
+router.get('/equipo', (req, res)  => {
+    res.render('links/equipo');
+});
+
+router.post('/equipo', (req, res ) => {
     res.send('received')
 });
 
+//estadisticas//
+router.get('/estadisticas', (req, res)  => {
+    res.render('links/estadisticas');
+});
+
+router.post('/estadisticas', (req, res ) => {
+    res.send('received')
+});
+
+//estadisticasequipo//
+router.get('/estadisticasequipo', (req, res)  => {
+    res.render('links/estadisticasequipo');
+});
+
+router.post('/estadisticasequipo', (req, res ) => {
+    res.send('received')
+});
 module.exports = router; 
