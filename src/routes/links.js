3ddaced37051530fa449c42527be101c6de7bbeb
Anthony Joel <anto777molina777@gmail.com>
2022-09-26 15:06:44 -0500

    delete

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,7 +7,6 @@ const pool = require("../database");
 router.get("/jugador", (req, res) => {
   res.render("links/jugador");
 });
-
 router.post("/jugador", async (req, res) => {
   const {
     nombrejugador,
@@ -24,20 +23,25 @@ router.post("/jugador", async (req, res) => {
     edadjugador
   };
   await pool.query('INSERT INTO jugador set ?', [newLink]);
-  res.send("received");
+  res.redirect("listajugador");
 });
-
+//mostrara las listas//
 router.get('/listajugador', async (req, res) =>{
   const jugador = await pool.query('SELECT *FROM  jugador');  
   res.render('links/list', {jugador})
 })
+//eliminar jugador//
+router.get('/delete/:id', async(req, res) =>{
+const { id } = req.params;
+await pool.query('DELETE FROM jugador WHERE ID = ?', [id]);
+res.redirect('/links/listajugador');
+});
 
 
 //equipo//
 router.get("/equipo", (req, res) => {
   res.render("links/equipo");
 });
-
 router.post("/equipo", async (req, res) => {
   const { equipo, dueño, entrenador, fecha } = req.body;
   const newLink = {
@@ -47,14 +51,20 @@ router.post("/equipo", async (req, res) => {
     fecha,
   };
   await pool.query('INSERT INTO equipo set ?' , [newLink]);
-  res.send("received");
+  res.redirect("listaequipo");
 });
-
+//mostrara las listas//
 router.get('/listaequipo', async (req, res) =>{
   const equipo = await pool.query('SELECT *FROM  equipo');  
   res.render('links/list1', {equipo})
 })
-
+//eliminar equipo//
+router.get('/delete/:id', async(req, res) =>{
+  const { id } = req.params;
+  await pool.query('DELETE FROM equipo WHERE ID = ?', [id]);
+  res.redirect('/links/listaequipo');
+  });
+  
 
 //estadisticas//
 router.get("/estadisticas", (req, res) => {
@@ -83,13 +93,19 @@ router.post("/estadisticas", async (req, res) => {
     fisico,
   };
   await pool.query('INSERT INTO estadisticas set ?', [newLink]);
-  res.send("received");
+  res.redirect("listaestadisticas");
 });
-
+//mostrara las listas//
 router.get('/listaestadisticas', async (req, res) =>{
   const estadisticas = await pool.query('SELECT *FROM  estadisticas');  
   res.render('links/list2', {estadisticas})
 })
+//eliminar
+router.get('/delete/:id', async(req, res) =>{
+  const { id } = req.params;
+  await pool.query('DELETE FROM estadisticas WHERE ID = ?', [id]);
+  res.redirect('/links/listaestadisticas');
+  });
 
 //estadisticasequipo//
 router.get("/estadisticasequipo", (req, res) => {
@@ -104,12 +120,18 @@ router.post("/estadisticasequipo", async (req, res) => {
     triunfosequipo,
   };
   await pool.query('INSERT INTO estadisticasequipo set ?', [newLink]);
-  res.send("received");
+  res.redirect("listaestadisticasequipo");
 });
+//mostrara las listas//
 router.get('/listaestadisticasequipo', async (req, res) =>{
   const estadisticasequipo = await pool.query('SELECT *FROM  estadisticasequipo');  
   res.render('links/list3', {estadisticasequipo})
 })
 
+router.get('/delete/:id', async(req, res) =>{
+  const { id } = req.params;
+  await pool.query('DELETE FROM estadisticasequipo WHERE ID = ?', [id]);
+  res.redirect('/links/listaestadisticasequipo');
+  });
 
 module.exports = router;
