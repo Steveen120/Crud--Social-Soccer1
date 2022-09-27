62352cd48396cb5694ab6c85a791bf73626d84a9
Anthony Joel <anto777molina777@gmail.com>
2022-09-27 17:27:12 -0500

    eliminar

--- src/routes/links.js
+++ src/routes/links.js
@@ -36,33 +36,6 @@ const { id } = req.params;
 await pool.query('DELETE FROM jugador WHERE ID = ?', [id]);
 res.redirect('/links/listajugador');
 });
-//Editar//
-router.get('/edit-jugador/:id', async (req, res) => {
-  const { id } = req.params;
-  const jugador = await pool.query ('SELECT * FROM jugador WHERE id = ?', [id]);  
-  console.log(jugador[0])
-  res.render ('links/edit-jugador' , {jugador: jugador[0]});
-});
-//se mostrara ya editado//
-router.post('/edit-jugador/:id', async ( req, res ) => {
-  const { id } = req.params;
-  const { nombrejugador,
-    posicionjugador,    
-    apellidojugador,
-    alturajugador,
-    edadjugador } = req.body;
-  const newLink = { nombrejugador,
-    posicionjugador,    
-    apellidojugador,
-    alturajugador,
-    edadjugador };
-  console.log(newLink);
-  await pool.query('UPDATE jugador set ? WHERE id = ?', [newLink, id]);
-  res.redirect('/links/listajugador');
-});
-
-
-
 
 
 //equipo//
@@ -91,29 +64,6 @@ router.get('/delete-equipo/:id', async(req, res) =>{
   await pool.query('DELETE FROM equipo WHERE ID = ?', [id]);
   res.redirect('/links/listaequipo');
   });
-  //Editar//
-router.get('/edit-equipo/:id', async (req, res) => {
-  const { id } = req.params;
-  const equipo = await pool.query ('SELECT * FROM equipo WHERE id = ?', [id]);  
-  console.log(equipo[0])
-  res.render ('links/edit-equipo' , {equipo: equipo[0]});
-});
-//se mostrara ya editado//
-router.post('/edit-equipo/:id', async ( req, res ) => {
-  const { id } = req.params;
-  const { equipo,
-    dueño,
-    entrenador,
-    fecha, } = req.body;
-  const newLink = { equipo,
-    dueño,
-    entrenador,
-    fecha, };
-  console.log(newLink);
-  await pool.query('UPDATE equipo set ? WHERE id = ?', [newLink, id]);
-  req.flash('éxito, comentario guardado correctamente');
-  res.redirect('/links/listaequipo');
-});
 
 
 
@@ -158,23 +108,6 @@ router.get('/delete-estadisticas/:id', async(req, res) =>{
   res.redirect('/links/listaestadisticas');
   });
 
-//Editar//
-router.get('/edit-estadisticas/:id', async (req, res) => {
-  const { id } = req.params;
-  const estadisticas = await pool.query ('SELECT * FROM estadisticas WHERE id = ?', [id]);  
-  console.log(estadisticas[0])
-  res.render ('links/edit-estadistica' , {estadisticas: estadisticas[0]});
-});
-
-router.post('/edit-estadisticas/:id', async ( req, res ) => {
-  const { id } = req.params;
-  const { id_jugador, id_estadisticas, ritmo, tiro, pase, agilidad, defensa, fisico } = req.body;
-  const newLink = { id_jugador, id_estadisticas, ritmo ,tiro, pase, agilidad, defensa, fisico };
-  console.log(newLink);
-  await pool.query('UPDATE estadisticas set ? WHERE id = ?', [newLink, id]);
-  res.redirect('/links/listaestadisticas');
-});
-
 
 //estadisticasequipo//
 router.get("/estadisticasequipo", (req, res) => {
@@ -202,20 +135,5 @@ router.get('/delete-estadisticasequipo/:id', async(req, res) =>{
   await pool.query('DELETE FROM estadisticasequipo WHERE ID = ?', [id]);
   res.redirect('/links/listaestadisticasequipo');
   });
-//Editar//
-router.get('/edit-estadisticasequipo/:id', async (req, res) => {
-  const { id } = req.params;
-  const estadisticasequipo = await pool.query ('SELECT * FROM estadisticasequipo WHERE id = ?', [id]);  
-  console.log(estadisticasequipo[0])
-  res.render ('links/edit-estadisticasequipo' , {estadisticasequipo: estadisticasequipo[0]});
-});
-//se mostrara ya editado//
-router.post('/edit-estadisticasequipo/:id', async ( req, res ) => {
-  const { id } = req.params;
-  const { valoracionequipo, quimicaequipo, triunfosequipo } = req.body;
-  const newLink = { valoracionequipo, quimicaequipo, triunfosequipo };
-  console.log(newLink);
-  await pool.query('UPDATE estadisticasequipo set ? WHERE id = ?', [newLink, id]);
-  res.redirect('/links/listaestadisticasequipo');
-});
+
 module.exports = router;
