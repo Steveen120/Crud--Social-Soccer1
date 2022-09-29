70bb035503ad20952fd3e3e0d1bbcafa3b78ae0d
Anthony Joel <anto777molina777@gmail.com>
2022-09-29 18:05:35 -0500

    name

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -2,13 +2,20 @@ const passport = require('passport');
 const passpor = require('passport'); 
 const LocalStrategy = require('passport-local').Strategy;
 
+
+
 passport.use('local.signup', new LocalStrategy ({
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true 
 }, async(req, username, password, done) => {
-    console.log(req.body);
-
+    const{ fullname } = req.body
+    const newUser = {
+        username,
+        password,
+        fullname
+    };
+    
 }));
 
 //password.serializeUser((usr,done) => {
